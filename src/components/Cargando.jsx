1e6566339b9db64bcd3df46c1c31857338595107
ElvisQL <quispeelvis7@gmail.com>
2023-09-20 10:41:15 -0300

    solucion gif cargando y netlify.toml

--- src/components/Cargando.jsx
+++ src/components/Cargando.jsx
@@ -1,9 +1,8 @@
-import loading from "../assets/Spinner-1s-200px.gif";
 
 export const Cargando = () => {
   return (
     <div className="container-loading">
-      <img src={loading} alt="Cargando..." />
+      <img src="https://firebasestorage.googleapis.com/v0/b/proyectofinalreactjs-elvis.appspot.com/o/images%2FSpinner-1s-200px.gif?alt=media&token=826dd004-53b8-4629-a9ea-3548e751b4e9" alt="Cargando..." />
       <span>Cargando</span>
     </div>
   );
